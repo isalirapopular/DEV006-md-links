a4fca4f866314d38c2ff6175d6b7afe06ae12682
Isabel Lira <isabell.lira@gmail.com>
2023-07-12 09:05:59 -0400

    test que deberia retornar una promesa que se resuelva en un array

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,38 +1,24 @@
 const { mdLinks } = require('../index.js');
 
-const path = 'Muestras/README.md'; 
-const options = { validate: true };
 
 describe('mdLinks', () => {
-  it('debería retornar una promesa que se resuelva con un array de objetos', (done) => {
-    const result = mdLinks(path, options);
-    expect(result).resolves.toEqual([
-      {
-        text: 'Linea de comando CLI',
-        url: 'https://medium.com/netscape/a-guide-to-create-a-nodejs-command-line-package-c2166ad0452e',
-        file: '/Users/isa/Library/CloudStorage/OneDrive-Personal/ISA/001.CURSOS/Laboratoria/04.Junio/DEV006-md-links/README.md',
-        status: 200,
-        ok: 'ok'
-      }
-    ]).then(done);
-  });
-});
+  it('debería retornar una promesa que se resuelva con un array de objetos', () => {
+    const path = 'Muestras/README.md';
+    const options = { validate: true };
 
-describe('mdLinks', () => {
-  it('debería identificar correctamente un archivo Markdown', (done) => {
-    const path = 'Muestras/README.md'; // 
+    return mdLinks(path, options).then((result) => {
+      // Verificar que el resultado sea un array
+      expect(Array.isArray(result)).toBe(true);
 
-    mdLinks(path, { validate: true })
-      .then((result) => {
-        expect(result).toEqual(expect.arrayContaining([
-          expect.objectContaining({
-            file: path,
-          }),
-        ]));
-        done();
-      })
-      .catch((error) => {
-        done(error);
+      // Verificar que cada elemento del array sea un objeto con las propiedades esperadas
+      result.forEach((link) => {
+        expect(link).toHaveProperty('text');
+        expect(link).toHaveProperty('url');
+        expect(link).toHaveProperty('file');
+        expect(link).toHaveProperty('status');
+        expect(link).toHaveProperty('ok');
       });
+    });
   });
 });
+
