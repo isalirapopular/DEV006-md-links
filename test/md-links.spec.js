ed05c2648e131c39280b93a5a3a10a0abdb3db5c
Isabel Lira <isabell.lira@gmail.com>
2023-07-13 10:18:17 -0400

    agrega un segundo test con mock para probar la funcion getFilesinDirectory  arroja 81 por ciento

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,5 +1,8 @@
 const { mdLinks } = require('../index.js');
+const { getFilesInDirectory } = require('../mdLinks.js');
 
+const axios = require('axios');
+const fs = require('fs');
 
 describe('mdLinks', () => {
   it('debería retornar una promesa que se resuelva con un array de objetos', () => {
@@ -22,3 +25,20 @@ describe('mdLinks', () => {
   });
 });
 
+describe('getFilesInDirectory index.js', () => {
+  it('Debe resolver con la lista de archivos Markdown dentro del directorio', () => {
+    const directoryPath = '/Users/isa/Library/CloudStorage/OneDrive-Personal/ISA/001.CURSOS/Laboratoria/04.Junio/DEV006-md-links/Muestras';
+
+    // Mock de fs.readdir para devolver archivos específicos
+    jest.spyOn(fs, 'readdir').mockImplementation((route, callback) => {
+      const files = ['Muestras/README.md'];
+      callback(null, files);
+    });
+
+    const expected = [
+      '/Users/isa/Library/CloudStorage/OneDrive-Personal/ISA/001.CURSOS/Laboratoria/04.Junio/DEV006-md-links/Muestras/Muestras/README.md',
+    ];
+
+    return expect(getFilesInDirectory(directoryPath)).resolves.toEqual(expected);
+  });
+});
